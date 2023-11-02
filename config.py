d7abad07f9765948ba48c2ba4cc475595aec174d
Archieland <archilirobakidze@gmail.com>
2023-11-02 20:16:14 +0400

    add config file

---
+++ config.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+CONFIG_JSON_PATH = 'config.json'
+
+with open(CONFIG_JSON_PATH, 'r') as config_file:
+    config_data = json.load(config_file)
+
+DB_PATH = config_data.get('DB_PATH')
+SENDGRID_EMAIL = config_data.get('SENDGRID_EMAIL')
+EMAILS = config_data.get('EMAILS')
+SENDGRID_API_KEY = config_data.get('SENDGRID_API_KEY')
