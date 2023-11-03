8bb3de78785fae9190dc30a661aa7db4d409bf70
Archieland <archilirobakidze@gmail.com>
2023-11-04 02:12:33 +0400

    fix empty email bug

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from clients import DatabaseClient, EmailClient
 from models import Website
 from parsers import Parser
 from config import URL, SEND_EMAIL
+import logging
+logging.basicConfig(filename='logs.log', level=logging.DEBUG, 
+                    filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 db_client = DatabaseClient()
 email_client = EmailClient()
@@ -16,7 +19,7 @@ products = db_client.get_products()
 new_products = list(filter(lambda x: not x in products, candidates))
 db_client.add_products(new_products)
 
-if SEND_EMAIL:
+if SEND_EMAIL and len(new_products):
     email_client.send_product_info(
         products=new_products
     )
