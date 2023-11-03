a016206f2f8e2e0f8a75b43bdf095ab45bf6611e
Archieland <archilirobakidze@gmail.com>
2023-11-04 00:37:37 +0400

    add xpath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from clients import DatabaseClient, EmailClient
 from models import Product, Website
-from parsers import Parser
+from parsers import Parser, MyAutoParser
 from config import URL
 
 # Initialize clients
@@ -10,17 +10,17 @@ email_client = EmailClient()
 # TODO, add these variables as cmd argument
 website = Website.detect_type_from_url(URL)
 
-parser = Parser.from_website(website=website)
-candidates = [] # parser.get_products(URL)
+parser = MyAutoParser()
+candidates = parser.get_products(URL)
 
 products = db_client.get_products()
 
 new_products = list(filter(lambda x: not x in products, candidates))
 db_client.add_products(new_products)
 
-if len(new_products) != len(products): # First time we don't want to send emails
+if len(products) and len(new_products): # First time we don't want to send emails
     email_client.send_product_info(
-        products=products
+        products=new_products
     )
 
 
--- parsers.py
+++ parsers.py
@@ -1,29 +1,46 @@
 from abc import abstractmethod
 from models import Product, Website
 from typing import List
+import requests
+from requests import Session
+import json
 
 class Parser:
-
-    def __init__(self, parser_type: Website) -> None:
-        self.parser_type = parser_type
 
     @classmethod
     def from_website(cls, website: Website):
         if website == Website.MYAUTO:
-            return MyAutoParser
+            return MyAutoParser()
         elif website == Website.MYHOME:
-            return MyHomeParser
+            # return MyHomeParser()
+            pass
         elif website == Website.SS:
-            return SSParser
+            # return SSParser()
+            pass
 
     @abstractmethod
     def get_products(self, url) -> List[Product]:
         pass
 
 class MyAutoParser(Parser):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+    }
 
-    def get_products(self, url) -> List[Product]:
-        raise NotImplementedError()
+    def get_products(self, url: str) -> List[Product]:
+        with Session() as session:
+            session.headers.update(self.headers)
+            response = session.get(url)
+        
+        if response.status_code != 200:
+                raise Exception(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        prelink = 'https://www.myauto.ge/ka/pr/'
+        items = json.loads(response.content)['data']['items']
+
+        products = [Product(website='myauto', url=prelink + str(item['car_id'])) for item in items]
+
+
+        return products
     
 class MyHomeParser(Parser):
 
